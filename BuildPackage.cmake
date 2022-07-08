73b5eba94eb99303e082575e532b808ea6623472
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-07-09 01:02:29 +0200

    Added required files.

---
+++ BuildPackage.cmake
@@ -0,0 +1,45 @@
+function(Build_VCPKG_Package vcpkg_libraries)
+	list(vcpkg_libraries LENGTH list_length)
+
+	if(list_length EQUAL 0)
+		message(FATAL_ERROR "Variable must be a non-empty list.")
+	endif()
+
+	find_program(Git)
+
+	set(vcpkg_dir ${CMAKE_CURRENT_BINARY_DIR}/vcpkg)
+
+	if(IS_DIRECTORY vcpkg_dir)
+		file(REMOVE_RECURSE vcpkg_dir)
+	endif()
+
+	message(STATUS "Downloading VCPKG...")
+
+	execute_process(
+		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		COMMAND Git clone "https://github.com/microsoft/vcpkg.git"
+	)
+
+	if(WIN32)
+		if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+			set(VCPKG_START ${vcpkg_dir}/bootstrap-vcpkg.bat)
+		else()
+			message(FATAL_ERROR "Compiler must be a MSVC on Win32 platform.\nCurrently is '${CMAKE_CXX_COMPILER_ID}'.")
+		endif()
+	else()
+		set(VCPKG_START ${vcpkg_dir}/bootstrap-vcpkg.sh)
+	endif()
+
+	message(STATUS "Installing VCPKG...")
+
+	execute_process(COMMAND ${VCPKG_START})
+
+	find_program(vcpkg_app NAMES vcpkg HINTS ${vcpkg_dir})
+
+	foreach(package_name IN vcpkg_libraries)
+		message(STATUS "vcpkg: Installing ${package_name}...")
+		execute_process(
+			COMMAND ${vcpkg_app} install ${package_name}
+		)
+	endforeach()
+endfunction()
---
+++ DownloadPackageFromGitHub.cmake
@@ -0,0 +1,3 @@
+function(Download_VCPKG_Package owner repo_name version_id)
+
+endfunction()
