e173becd3fffd20dc7abf14878fb48b7dd873d74
Mateusz Okrój <mateuszokroj1@gmail.com>
2022-07-14 00:18:35 +0200

    Action is finished.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('child_process');
+const os = require('os');
 
 try {
     console.log("VCPKG automated configurator\n");
     const token = core.getInput("GITHUB_TOKEN");
+
+    core.setCommandEcho(true);
 
     const packages = core.getMultilineInput('out-package-id');
     if (packages.length < 1) {
@@ -12,12 +15,45 @@ try {
         return;
     }
 
-    const process = exec.execSync("git clone 'https://github.com/microsoft/vcpkg'");
-    
+    console.log("Downloading VCPKG...")
+    const process = exec.exec("git clone 'https://github.com/microsoft/vcpkg'");
+    if (process.exitCode != 0) {
+        core.setFailed("Error while downloading VCPKG.");
+    }
 
-    //const api = github.getOctokit(token);
-    //const { context } = github;
-    //context.
+    const api = github.getOctokit(token);
+    const platform = os.platform();
+
+    console.log("Installing VCPKG...");
+    if (platform == 'win32') {
+        const process = exec.exec("vcpkg/bootstrap_vcpkg.bat");
+        if (process.exitCode != 0) {
+            core.setFailed("Error while installing VCPKG.");
+        }
+    }
+    else {
+        const process = exec.exec("vcpkg/bootstrap_vcpkg.sh");
+        if (process.exitCode != 0) {
+            core.setFailed("Error while installing VCPKG.");
+        }
+    }
+
+    packages.forEach(name =>
+    {
+        console.log("vcpkg - installing " + name + "...");
+        if (platform == 'win32') {
+            const process = exec.exec("vcpkg/vcpkg.exe install " + name);
+            if (process.exitCode != 0) {
+                core.setFailed("Error while installing VCPKG.");
+            }
+        }
+        else {
+            const process = exec.exec("vcpkg/vcpkg install " + name);
+            if (process.exitCode != 0) {
+                core.setFailed("Error while installing VCPKG.");
+            }
+        }
+    });
 }
 catch (error) {
     core.setFailed(error.message);
