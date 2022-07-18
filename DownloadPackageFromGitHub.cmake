392557e631fa8dcc0aff8701c454c4967b4d0fed
Mateusz Okrój <mateuszokroj1@gmail.com>
2022-07-18 16:26:28 +0200

    Added CMake function. Connected to GitHub API.

--- DownloadPackageFromGitHub.cmake
+++ DownloadPackageFromGitHub.cmake
@@ -1,3 +1,26 @@
 function(Download_VCPKG_Package owner repo_name version_id)
+	if(owner MATCHES "^[A-z0-9_\\.-]+$")
+		message("Owner name is correct.")
+	else()
+		message(FATAL_ERROR "Owner name is not correct.")
+	endif()
+
+	if(repo_name MATCHES "^[A-z0-9_\\.-]+$")
+		message("Repository name is correct.")
+	else()
+		message(FATAL_ERROR "Repository name is not correct.")
+	endif()
+
+	if(version_id MATCHES "^[A-z0-9_-]+$")
+		message("Version Id is correct.")
+	else()
+		message(FATAL_ERROR "Version Id is not correct.")
+	endif()
+
+	message(STATUS "Connecting to GitHub API...")
+
+	file(DOWNLOAD "https://api.github.com/repos/${owner}/${repo_name}/actions/artifacts" ${CMAKE_CURRENT_LIST_DIR}/index_json)
+	file(STRINGS ${CMAKE_CURRENT_LIST_DIR}/index_json json_content)
+	message("${json_content}")
 
 endfunction()
