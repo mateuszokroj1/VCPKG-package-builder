424550a884cd928dfc17a9f5bf1c12190cd2b9d9
Mateusz Okrój <mateuszokroj1@gmail.com>
2022-07-19 17:05:08 +0200

    Finished download script.

--- DownloadPackageFromGitHub.cmake
+++ DownloadPackageFromGitHub.cmake
@@ -1,4 +1,4 @@
-function(Download_VCPKG_Package owner repo_name version_id)
+function(Download_VCPKG_Package owner repo_name version_id destination_dir)
 	if(owner MATCHES "^[A-z0-9_\\.-]+$")
 		message("Owner name is correct.")
 	else()
@@ -19,21 +19,50 @@ function(Download_VCPKG_Package owner repo_name version_id)
 
 	message(STATUS "Connecting to GitHub API...")
 
-	if(CMAKE_SYSTEM_NAME equal "Windows")
+	if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
 		set(target_platform "Windows")
-	elseif(CMAKE_SYSTEM_NAME equal "Darwin")
-		set(target_platform "MacOS")
+	elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+		set(target_platform "macOS")
 	else()
 		set(target_platform "Linux")
 	endif()
 
-	file(DOWNLOAD "https://api.github.com/repos/${owner}/${repo_name}/actions/artifacts/${version-id}-${target_platform}" ${CMAKE_CURRENT_LIST_DIR}/index_json)
-	file(STRINGS ${CMAKE_CURRENT_LIST_DIR}/index_json json_content)
-	
-	string(LENGTH json_content json_length)
+	set(target_artifact_name "${version_id}-${target_platform}")
+	message("Artifact name is: ${target_artifact_name}.")
 
-	if(json_length LESS 1)
+	file(DOWNLOAD "https://api.github.com/repos/${owner}/${repo_name}/actions/artifacts" ${CMAKE_CURRENT_LIST_DIR}/index_json)
+	file(READ ${CMAKE_CURRENT_LIST_DIR}/index_json json_content)
+	
+	if(NOT json_content)
 		message(FATAL_ERROR "GitHub API returned empty response. Maybe artifact not exist.")
 	endif()
 
+	string(JSON artifacts_count GET ${json_content} "total_count")
+	message("Found ${artifacts_count} items.")
+
+	string(JSON artifacts_list GET ${json_content} "artifacts")
+	
+	message("${artifacts_list}")
+
+	set(zip_url "")
+	foreach(item_index RANGE ${artifacts_count})
+		string(JSON current_name GET ${artifacts_list} ${item_index} "name")
+
+		message("Name of ${item_index} is ${current_name}.")
+
+		if(${current_name} STREQUAL ${target_artifact_name})
+			string(JSON zip_url GET ${artifacts_list} ${item_index} "archive_download_url")
+			break()
+		endif()
+	endforeach()
+	
+	string(LENGTH ${zip_url} url_length)
+	if(${url_length} LESS 4)
+		message(FATAL_ERROR "Cannot read Archive URL from GitHub API.")
+	endif()
+
+	message("${zip_url}")
+
+	set(downloaded_archive_name "${target_artifact_name}.zip")
+	file(DOWNLOAD ${zip_url} ${CMAKE_CURRENT_LIST_DIR}/${downloaded_archive_name})
 endfunction()
